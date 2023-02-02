c1b2e6053e78d7a84f0e66c8f8be891d7bef7150
Bobby De Luna <bobby.deluna2@gmail.com>
2023-02-02 00:59:25 -0700

    Completed Assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  resources :plants, only: [:index, :show, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+#Index route
+  get '/plants', to: 'plants#index'
+
+#Show route
+get '/plants/:id', to:'plants#show'
+
+#Create route
+
 end
